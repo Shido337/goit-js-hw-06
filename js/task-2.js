ed969904753c74df9d5ff86a688136b9c371a568
Русальчук Андрей <anrusalchuk@gmail.com>
2025-11-06 23:19:36 +0200

    :

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const index = this.items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems());
---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+function StringBuilder(initialValue) {
+  let value = initialValue;
+
+  this.getValue = function () {
+    return value;
+  };
+
+  this.padEnd = function (str) {
+    value += str;
+  };
+
+  this.padStart = function (str) {
+    value = str + value;
+  };
+
+  this.padBoth = function (str) {
+    value = str + value + str;
+  };
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
